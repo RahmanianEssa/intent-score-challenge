86a83670992a0a149299e15471db435ef97db21d
RahmanianEssa <rahmanianessa1@gmail.com>
2020-03-04 16:52:22 +0700

    progress latihan2

--- app/src/main/java/id/putraprima/skorbola/MatchActivity.java
+++ app/src/main/java/id/putraprima/skorbola/MatchActivity.java
@@ -1,5 +1,6 @@
 package id.putraprima.skorbola;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,11 +12,15 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MatchActivity extends AppCompatActivity {
+    public static final int SECOND_ACTIVITY_REQUEST_CODE_HOME= 1;
+    public static final int SECOND_ACTIVITY_REQUEST_CODE_AWAY = 2;
     private static final String HASIL_KEY = "hasil";
     private TextView homeText;
     private TextView awayText;
     private ImageView imageHome;
     private ImageView imageAway;
+    private TextView scorehome;
+    private TextView scoreaway;
     int skorHome = 0;
     int skorAway = 0;
 
@@ -28,6 +33,8 @@ public class MatchActivity extends AppCompatActivity {
         awayText = findViewById(R.id.txt_away);
         imageHome = findViewById(R.id.home_logo);
         imageAway = findViewById(R.id.away_logo);
+        scorehome = findViewById(R.id.score_home);
+        scoreaway = findViewById(R.id.score_away);
 
 
         Bundle extras = getIntent().getExtras();
@@ -46,18 +53,53 @@ public class MatchActivity extends AppCompatActivity {
         }
     }
 
+
     public void addhomescore(View view) {
         Intent intent = new Intent(this, ScorerActivity.class);
-        startActivity(intent);
+        startActivityForResult(intent, SECOND_ACTIVITY_REQUEST_CODE_HOME);
     }
 
     public void addawayscore(View view) {
         Intent intent = new Intent(this, ScorerActivity.class);
-        startActivity(intent);
+        startActivityForResult(intent, SECOND_ACTIVITY_REQUEST_CODE_AWAY);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SECOND_ACTIVITY_REQUEST_CODE_HOME) {
+            String returnString = data.getStringExtra(ScorerActivity.KEY_SCORER);
+            TextView tvHome = findViewById(R.id.textHome);
+            String scorerHome ="";
+            scorerHome += "\n"+ returnString;
+            tvHome.setText(scorerHome);
+            skorHome++;
+            scorehome.setText(String.valueOf(scorehome));
+        }
+        else if(requestCode == SECOND_ACTIVITY_REQUEST_CODE_AWAY){
+            String returnString = data.getStringExtra(ScorerActivity.KEY_SCORER);
+            TextView tvAway = findViewById(R.id.textAway);
+            String scorerAway ="";
+            scorerAway += "\n" + returnString;
+            tvAway.setText(scorerAway);
+            skorAway++;
+            scoreaway.setText(String.valueOf(scoreaway));
+        }
     }
 
     public void cekhasil(View view) {
+        String hasil = null;
+        if (skorHome==skorAway){
+            hasil="Draw";
+        } else if(skorHome>skorAway){
+            hasil = homeText.getText().toString();
+        } else if (skorAway>skorHome){
+            hasil=awayText.getText().toString();
+        }
         Intent intent = new Intent(this, ResultActivity.class);
+        intent.putExtra(HASIL_KEY, hasil);
         startActivity(intent);
+
+
     }
 }
--- app/src/main/java/id/putraprima/skorbola/ScorerActivity.java
+++ app/src/main/java/id/putraprima/skorbola/ScorerActivity.java
@@ -2,13 +2,26 @@ package id.putraprima.skorbola;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class ScorerActivity extends AppCompatActivity {
-
+public static final String KEY_SCORER = "upin";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scorer);
+    }
+
+    public void handleOk(View view) {
+        EditText editText = (EditText) findViewById(R.id.tvScorer);
+        String stringToPassBack = editText.getText().toString();
+
+        Intent intent = new Intent();
+        intent.putExtra(KEY_SCORER, stringToPassBack);
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
